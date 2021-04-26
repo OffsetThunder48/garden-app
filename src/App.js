9c9e50c05c1d01c126b1013a1c357196e4def74c
OffsetThunder48 <lonewolf2k@protonmail.com>
2021-04-26 17:08:04 +0530

    Initial App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavLink, Route, BrowserRouter, Switch } from 'react-router-dom';
+
+import { Home } from './homepage/home'
+import { Login } from './login/login'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Garden City</h1>
+          <p className="lead">Welcome to the Garden City</p>
+          </div>
+      </div>
+      <nav className="navbar-expand-md">
+          <ul className="navbar-nav navbar nav-fill w-100">
+            <NavLink className="style nav-item d-flex justify-content-center align-items-center" to="home">Home</NavLink>
+            <NavLink className="style nav-item d-flex justify-content-center align-items-center" to="login">Login</NavLink>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/homepage/home.js
@@ -0,0 +1,5 @@
+export const Home = () => {
+    return (
+        <h1>Hey, how are you</h1>
+    );
+};
